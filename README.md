99934aac5fdc8489106a540e1ee6d25128de93dc
smitcco <smithcodyc@yahoo.com>
2026-04-27 13:46:54 -0400

    File to run the model and output png results to appropriate directory

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ A team effort to create a ML model(s) as our CS325 Final Project.
     Our model(s) aim to predict a runner's final marathon time given certain prior information
         - prior info includes previous running mileage, previous running PRs, effort level, etc...
 
+    To run the model:
+        python run.py 
+
 
---
+++ run_model.py
@@ -0,0 +1,19 @@
+" Run model "
+
+import matplotlib
+import runpy
+matplotlib.use('Agg')
+
+print("\n-- Step 1: Data Cleaning/Preprocessing -- ")
+runpy.run_path('src/data_processing.py')
+
+print("\n-- Step 2: Model Selection/Training -- ")
+runpy.run_path('src/model_select_training.py')
+
+print("\n-- Step 3: Feature Selection -- ")
+runpy.run_path('src/feature_selection.py')
+
+print("\n-- Step 4: Model Eval -- ")
+runpy.run_path('src/model_evaluation.py')
+
+print("\nRuns done. Results located in results/")
